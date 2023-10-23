ee1abff019b3816186b0752d280d73afe990ef24
EdwinACL831 <eacl.831@gmail.com>
2023-10-23 12:22:38 -0400

    First Commit
    This is my first commit. Add the HTML, JS, and CSS files, plus a folder with mp3 files

---
+++ index.js
@@ -0,0 +1,31 @@
+let progressInterval;
+let progressElement;
+let progressBar;
+let currentProgress;
+let audioFile;
+
+function initJSVariables() {
+    progressElement = document.getElementById("progress");
+    progressBar = document.getElementById("pb");
+    audioFile = document.getElementById("audioFile");
+    currentProgress = parseInt(progressElement.innerHTML);
+}
+
+function progress() {
+    audioFile.play()
+    progressInterval = setInterval(() => {
+        if(currentProgress < 100) {
+            currentProgress += 20;
+            progressElement.innerHTML = currentProgress + '%';
+            progressBar.style.width = currentProgress + '%';
+        }
+    }, 1000);
+}
+
+function reset() {
+    currentProgress = 0;
+    let zeroP = currentProgress + '%';
+    progressElement.innerHTML = zeroP;
+    progressBar.style.width = zeroP;
+    clearInterval(progressInterval);
+}
