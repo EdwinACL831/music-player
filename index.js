be792f54a0d950f82971d6b09825cf09e95f63d1
Edwin A. Collazos <eacl.831@gmail.com>
2023-10-23 22:59:58 -0400

    Adds inline code comments and remove some unnecessary variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let progressInterval;
-let progressBar;
-let currentProgress;
-let audioFile;
+let progressInterval;           // variable to handle the progressbar filling animation
+let progressBar;                // variable that represents the progressbar HTML element
+let currentProgress;            // variable that represents the current progress of the song that is playing
+let audioFile;                  // variable that represents the audio HTML element
 
 function initJSVariables() {
     progressBar = document.getElementById("pb");
@@ -18,8 +18,9 @@ function printAudioCurrentTime(audioFile) {
 }
 
 function progress() {
-    audioFile.play()
-    let step = 100 / parseInt(audioFile.duration);
+    audioFile.play();
+    // This code block is to filling the progress bar in the GUI.
+    // this is executed each 1000ms until a clearInterval(progressInterval) call happens
     progressInterval = setInterval(() => {
         if(audioFile.currentTime <= audioFile.duration) {
             currentProgress = 100 * (audioFile.currentTime / audioFile.duration);
@@ -29,13 +30,17 @@ function progress() {
 }
 
 function reset() {
-    currentProgress = 0;
-    let zeroP = currentProgress + '%';
-    progressBar.style.width = zeroP;
-    clearInterval(progressInterval);
+    // reset the progressbar un the GUI to 0, so start from beginning
+    currentProgress = 0;          
+    progressBar.style.width = currentProgress + '%';
+    // restarts the song from the beginning
+    audioFile.currentTime = 0;
+    // Stop moving the progressbar un the GUI
+    clearInterval(progressInterval);    
 }
 
 function stop() {
+    // Stop moving the progressbar un the GUI
     clearInterval(progressInterval);
     audioFile.pause();
 }
